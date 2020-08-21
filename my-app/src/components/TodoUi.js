fbae34784c43ac1c0089501964b48662d399e3be
Stephen Guo <wetech16@gmail.com>
2020-08-20 22:41:29 -0700

    -add

--- my-app/src/components/TodoUi.js
+++ my-app/src/components/TodoUi.js
@@ -1,12 +1,17 @@
 import React, {useState, useRef} from 'react'
 import TodoList from './TodoList'
-
+import uuidv4 from 'uuid/v4'
 
 export default function TodoUi() {
     const [todos, setTodos] = useState([{ id: 1, name: 'Todo 1' , complete: false}])
     const todoRef = useRef()
     function handleAddTodo () {
-        
+        const name = todoRef.current.value
+        if (name ==='') return
+        setTodos(prev => {
+            return [...prev, {id: uuidv4(), name:name, complete:false}]
+        })
+        todoRef.current.value = null;
     }
 
     return (
