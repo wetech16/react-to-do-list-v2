d4baa8ec30e2adebacb4e9e995530a52a72e2914
Stephen Guo <wetech16@gmail.com>
2020-08-20 23:45:27 -0700

    -add clear

--- my-app/src/components/Todo.js
+++ my-app/src/components/Todo.js
@@ -1,10 +1,15 @@
 import React from 'react'
 
-export default function Todo({todo}) {
-
+export default function Todo({todo, toggleTodo}) {
+    function handleTodoClick () {
+        toggleTodo(todo.id)
+    }
     return (
         <div>
-            {todo.name}
+            <label>
+                <input type="checkbox" checked ={todo.complete} onChange={handleTodoClick}/>
+                {todo.name} 
+            </label>
         </div>
             
             
--- my-app/src/components/TodoUi.js
+++ my-app/src/components/TodoUi.js
@@ -17,6 +17,13 @@ export default function TodoUi() {
        localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos))
     }, [todos])
 
+    function toggleTodo(id) {
+        const newTodos = [...todos]
+        const todo = newTodos.find(todo => todo.id === id)
+        todo.complete = !todo.complete
+        setTodos(newTodos)
+    }
+
     function handleAddTodo () {
         const name = todoRef.current.value
         if (name ==='') return
@@ -26,13 +33,18 @@ export default function TodoUi() {
         todoRef.current.value = null;
     }
 
+    function handleClearTodos () {
+        const newTodos = todos.filter(todo => !todo.complete)
+        setTodos(newTodos)
+    }
+
     return (
         <div className="App-header">
-            <TodoList todos={todos} />
+            <TodoList todos={todos} toggleTodo={toggleTodo}/>
             <input ref={todoRef} type="text" />
             <button onClick={handleAddTodo}>Add Todo</button>
-            <button>CLear Complete</button>
-            <div>0 left to do</div>
+            <button onClick={handleClearTodos}>CLear Complete</button>
+            <div>{todos.filter(todo => !todo.complete).length} left to do</div>
         </div>
     )
 }
